548d636ed9587656ab49b1e5b6472c82c9f01654
Keemchard <86994854+Keemchard@users.noreply.github.com>
2022-02-14 15:52:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// let btn = document.querySelector(".btn");
+// let error = document.querySelector(".error");
+
+// btn,
+//   addEventListener("click", function () {
+//     error.style.display = "flex";
+//   });
